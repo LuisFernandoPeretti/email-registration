c10232d15e6496f7b103f6cd7b5f480c1434c75e
Luis Fernando <nando.lfp@hotmail.com>
2022-01-14 23:50:17 -0300

    connect pg database and server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//routes
+
+//create a register
+
+//get all register
+
+//get a register
+
+//delete a register
 
 app.listen(5000, () => {
     console.log("Server has startes on port 5000");
